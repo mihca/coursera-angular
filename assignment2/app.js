9740c5fc83383ac6167501943e2e5d9ddcb39ec3
muellera <achim.mueller@msg-systems.com>
2017-03-13 10:33:11 +0100

    Assignent 2

---
+++ assignment2/app.js
@@ -0,0 +1,71 @@
+(function () {
+    'use strict';
+
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ToBuyController', ToBuyController)
+        .controller('AlreadyBoughtController', AlreadyBoughtController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController (ShoppingListCheckOffService) {
+
+        var controller = this;
+
+        controller.everythingBought = function () {
+            return ShoppingListCheckOffService.getItemsToBuy().length === 0;
+        }
+
+        controller.buyItem = function buyItem (itemIndex) {
+            ShoppingListCheckOffService.buyItem(itemIndex)
+        }
+
+        controller.getItemsToBuy = function () {
+            return ShoppingListCheckOffService.getItemsToBuy();
+        }
+    }
+
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController (ShoppingListCheckOffService) {
+
+        var controller = this;
+
+        controller.nothingBought = function () {
+            return ShoppingListCheckOffService.getItemsAlreadyBought().length === 0;
+        }
+
+        controller.getItemsAlreadyBought = function () {
+            return ShoppingListCheckOffService.getItemsAlreadyBought();
+        }
+    }
+
+    function ShoppingListCheckOffService () {
+        var service = this;
+
+        var itemsToBuy = [
+            {
+                name: "Cookies",
+                quantity: 10
+            },
+            {
+                name: "Milk",
+                quantity: 1
+            }
+        ];
+
+        var itemsAlreadyBought = [];
+
+        service.buyItem = function (itemIndex) {
+            itemsAlreadyBought.push(itemsToBuy[itemIndex]);
+            itemsToBuy.splice(itemIndex, 1);
+        };
+
+        service.getItemsToBuy = function () {
+            return itemsToBuy;
+        };
+
+        service.getItemsAlreadyBought = function () {
+            return itemsAlreadyBought;
+        };
+    }
+
+})();
